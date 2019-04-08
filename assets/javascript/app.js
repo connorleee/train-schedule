a85b6970ecab83ad1549e96e5e35937458d4e0f6
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-04-07 23:05:05 -0700

    Created event handler that looks for child changes in the database and writes to the train schedule. added some to do's

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,6 +15,55 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-$("#form-submit").click(function () {
+var trainName = "";
+var destination = "";
+var firstTrain = "";
+var departureFreq = "";
 
+// Populate database with user inputs
+$("#form-submit").click(function () {
+    trainName = $("#train-name").val().trim();
+    destination = $("#destination").val().trim();
+    firstTrain = $("#first-departure").val().trim();
+    departureFreq = $("#departure-frequency").val().trim();
+
+    // Push the values in the form to the database
+    database.ref("/trainInfo").push({
+        trainName: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        departureFreq: departureFreq
+    });
+
+    // Clear the textboxes
+    $("#train-name").val("");
+    $("#destination").val("");
+    $("#first-departure").val("");
+    $("#departure-frequency").val("");
 })
+
+database.ref("/trainInfo").on("child_added", function(childSnapshot){
+    console.log(childSnapshot.val())
+
+    var trainNameRecent = childSnapshot.val().trainName;
+    var destinationRecent = childSnapshot.val().destination;
+    var firstTrainRecent = childSnapshot.val().firstTrain;
+    var departureFreqRecent = childSnapshot.val().departureFreq;
+
+    // TODO: funciton to calculate the next arrival and the minutes away
+    // TODO: figure out a better way of controlling how the time is displayed and received by the user input.
+
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainNameRecent),
+        $("<td>").text(destinationRecent),
+        $("<td>").text(departureFreqRecent),
+        // $("<td>").text(nextArrival),
+        // $("<td>").text(minutesAway)
+
+    )
+    
+    $("#train-table").append(newRow);
+})
+
+// TODO: figure out way to reset the train schedule if wanted to 
+// TODO: have the train schedule timers automatically update over time
